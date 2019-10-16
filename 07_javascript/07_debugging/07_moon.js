a5d2c7dfce4d3d9c49c6a1e5466b6bee1e595160
wlotherington <dev@kaplop.com>
2019-10-15 19:24:40 -0500

    add course files

---
+++ 07_javascript/07_debugging/07_moon.js
@@ -0,0 +1,25 @@
+var species = ['wolf', 'human', 'wasp', 'squirrel', 'weasel', 'dinosaur'];
+var isMidnight = true;
+var isFullmoon = true;
+
+function isTransformable(species) {
+  return species[0] === 'w';
+}
+
+function transform(species) {
+  return 'were' + species;
+}
+
+var i;
+for (i = 0; i < species.length; i++) {
+  var thisSpecies = species[i];
+  var newSpecies = false;
+
+  if (isMidnight && isFullmoon && isTransformable(thisSpecies)) {
+      newSpecies = transform(thisSpecies);
+  }
+
+  if (newSpecies) {
+    console.log('Beware of the ' + newSpecies + '!');
+  }
+}
