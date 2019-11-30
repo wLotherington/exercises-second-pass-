43370d16c1aa754a5ac7a3146fe81c9b3e7963bc
wlotherington <dev@kaplop.com>
2019-11-30 16:20:37 -0600

    add course exercise files

---
+++ 08_js210/08_medium/01_rotation.js
@@ -0,0 +1,26 @@
+function rotateArray(arr) {
+  if (!Array.isArray(arr)) {
+    return undefined;
+  } else if (arr.length === 0) {
+    return [];
+  }
+
+  return arr.slice(1).concat(arr[0]);
+}
+
+console.log(rotateArray([7, 3, 5, 2, 9, 1]));       // [3, 5, 2, 9, 1, 7]
+console.log(rotateArray(['a', 'b', 'c']));          // ["b", "c", "a"]
+console.log(rotateArray(['a']));                    // ["a"]
+console.log(rotateArray([1, 'a', 3, 'c']));         // ["a", 3, "c", 1]
+console.log(rotateArray([{ a: 2 }, [1, 2], 3]));    // [[1, 2], 3, { a: 2 }]
+console.log(rotateArray([]));                       // []
+
+// return `undefined` if the argument is not an array
+console.log(rotateArray());                         // undefined
+console.log(rotateArray(1));                        // undefined
+
+
+// the input array is not mutated
+var array = [1, 2, 3, 4];
+console.log(rotateArray(array));                    // [2, 3, 4, 1]
+console.log(array);                                 // [1, 2, 3, 4]
