0e8aeff1b04e35185cb22675c3036ec43ccf322d
wlotherington <dev@kaplop.com>
2020-01-23 17:29:16 -0600

    add exercise files

--- 09_js225/08_practice_this.js
+++ 09_js225/08_practice_this.js
@@ -1,10 +1,22 @@
-var numbers = [1, 2, 3, 4];
-function makeCheckEven() {
-  return function(num) {
-    return num % 2 === 0;
-  }
+// var numbers = [1, 2, 3, 4];
+// function makeCheckEven() {
+//   return function(num) {
+//     return num % 2 === 0;
+//   }
+// }
+
+// var checkEven = makeCheckEven();
+
+// console.log(numbers.filter(checkEven)); // [2, 4]
+
+function execute(func, operand) {
+  // ... implement this function
 }
 
-var checkEven = makeCheckEven();
+execute(function(number) {
+  return number * 2;
+}, 10); // 20
 
-console.log(numbers.filter(checkEven)); // [2, 4]
+// execute(function(string) {
+//   return string.toUpperCase();
+// }, 'hey there buddy'); // "HEY THERE BUDDY"
